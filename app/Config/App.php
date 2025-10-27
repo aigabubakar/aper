5b81ad2797240b1d6ec4be5520c3370f8132f9d0
aigabubakar <onearena4ng@gmail.com>
2025-10-27 16:25:40 +0100

    update commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -15,9 +15,12 @@ class App extends BaseConfig
      * WITH a trailing slash:
      *
      * E.g., http://example.com/
+     * 
+     *  public string $baseURL = 'http://localhost:8080/';
      */
-    public string $baseURL = 'http://localhost:8080/';
-
+    public string $baseURL = 'https://www.edouniversity.edu.ng/aper/';
+    
+    
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
      * If you want to accept multiple Hostnames, set this.
